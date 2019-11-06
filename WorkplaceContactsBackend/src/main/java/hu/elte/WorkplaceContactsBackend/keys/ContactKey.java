14ab1e69ccabe51beefe33de36034e3500f93551
Nagy Zoltán Attila <emsgem@INF.ELTE.HU>
2019-11-06 15:41:17 +0100

    Try fix Contac post (broken)

--- WorkplaceContactsBackend/src/main/java/hu/elte/WorkplaceContactsBackend/keys/ContactKey.java
+++ WorkplaceContactsBackend/src/main/java/hu/elte/WorkplaceContactsBackend/keys/ContactKey.java
@@ -2,10 +2,14 @@ package hu.elte.WorkplaceContactsBackend.keys;
 
 import hu.elte.WorkplaceContactsBackend.entities.Person;
 import java.io.Serializable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 public class ContactKey implements Serializable  {
     private Person person;
